225fde566dca05c0678cfde0ece3e15fab7432f5
Martin Feineis <mfeineis@users.noreply.github.com>
2021-05-14 18:47:36 +0200

    refactor: Simplify directory layout

---
+++ lib/Exploration.fs
@@ -0,0 +1,12 @@
+namespace Exploration
+
+open System
+
+// [<CLIMutable>]
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32.0 + (float this.TemperatureC / 0.5556)
